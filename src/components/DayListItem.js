3d261de7571e9c7adeaf400d5254d7cead56580e
Martha Staus <mjstaus@gmail.com>
2022-02-23 16:56:51 -0800

    Add syling for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
-  
   const day = props.name;
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
 
   return (
-    <li onClick={() => props.setDay(day)}>
+    <li 
+      className={ dayClass }
+      onClick={() => props.setDay(day)}>
       <h2  className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots}</h3>
     </li>
