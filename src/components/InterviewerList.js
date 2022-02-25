ee78ef618409780c1ca716b57886978d081902e4
Martha Staus <mjstaus@gmail.com>
2022-02-24 16:36:40 -0800

    Add InterviewerList component, styles, story

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList({ interviewers, setInterviewer, interviewer}) {
+
+  const interviewerList = interviewers.map(({ id, name, avatar }) => (
+    <InterviewerListItem
+    key={id}
+    name={name}
+    avatar={avatar} 
+    selected={id === interviewer}
+    setInterviewer={setInterviewer}
+    />
+))
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"> {interviewerList}</ul>
+    </section>
+  );
+}
