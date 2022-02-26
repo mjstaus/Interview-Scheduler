21370420dcbea0ec2d608463dcc3222edbe23983
Martha Staus <mjstaus@gmail.com>
2022-02-26 08:02:46 -0800

    Add Show component + story

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,3 +147,4 @@ storiesOf("DayList", module)
       storiesOf("Appointment", module)
       .add("Header", () => <Header time="12pm"/>)
       .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+      .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers[0]} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
