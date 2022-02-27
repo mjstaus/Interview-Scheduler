45a2c6afe345c83b70b8b8ad03cde2e70e7faf2e
Martha Staus <mjstaus@gmail.com>
2022-02-27 10:12:07 -0800

    Add Appointment component and test data  to Application

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.js"
+import Show from "components/Appointment/Show.js"
+import Empty from "components/Appointment/Empty.js"
 
-export default function Appointment({time}) {
+export default function Appointment({ interview={}, time }) {
+  const { student, interviewer } = interview;
+
 
   return (
-    <article className="appointment">{time}</article>
+    <article className="appointment">
+      <Header time={time}/>
+      { student && interviewer ? <Show student={student} interviewer={interviewer}/> : <Empty/> }
+    </article>
+    
   )
 }
