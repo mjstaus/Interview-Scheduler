103b307604c07d647eb1e9f354d320e1627d68b6
Martha Staus <mjstaus@gmail.com>
2022-03-01 10:02:44 -0800

    Refactor setDay and setDays, now consolidated into setState

--- src/components/Application.js
+++ src/components/Application.js
@@ -43,10 +43,17 @@ const appointments = {
   }
 };
 
-export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
 
+export default function Application(props) {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  
   useEffect(() => {
     axios
       .get("/api/days", {
@@ -76,8 +83,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
