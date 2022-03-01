883d93bdeb13bf8ab6aede1d6f62e1280260b71c
Martha Staus <mjstaus@gmail.com>
2022-03-01 10:26:22 -0800

    remove nullish coalescing operators

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,14 @@
 export function getAppointmentsForDay(state, day) {
-  const apptsFromDay = state.days.filter(d => d.name === day)[0]?.appointments;
+  const apptsFromDay = state.days.filter(d => d.name === day)[0].appointments;
   
   if(!apptsFromDay){
     return [];
   }
   
-  const apptsArray = Object.entries(state?.appointments);
+  const apptsArray = Object.entries(state.appointments);
   const result = [];
   for(const appt of apptsFromDay){
-    const a = apptsArray.filter(a => a[1]?.id === appt);
+    const a = apptsArray.filter(a => a[1].id === appt);
     result.push(a[0][1]);
   }
   return result;
