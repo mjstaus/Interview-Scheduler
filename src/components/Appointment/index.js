375900ad5c100642d17bfea6b6b8d87316d33ea6
Martha Staus <mjstaus@gmail.com>
2022-03-01 21:10:25 -0800

    Add useVisualMode hook and modes (empty, show, create, saving)

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header.js"
-import Show from "components/Appointment/Show.js"
-import Empty from "components/Appointment/Empty.js"
+import Header from "components/Appointment/Header.js";
+import Show from "components/Appointment/Show.js";
+import Empty from "components/Appointment/Empty.js";
+import Form from "components/Appointment/Form.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
+import Confirm from "components/Appointment/Confirm.js";
+import { useVisualMode } from "hooks/useVisualMode";
+
+const SHOW = "SHOW";
+const EMPTY = "EMPTY";
+const CREATE = "CREATE";
+const EDIT = "EDIT";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment({ interview, time }) {
+  
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={time}/>
-      { interview && interview.student && interview.interviewer ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty/> }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer} />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]}
+          onSave={() => transition(SAVING)}
+          onCancel={() => back()}
+          />)}
+      {mode === SAVING && (
+        <Status message="Saving"/>
+      )}
     </article>
-    
   )
 }
