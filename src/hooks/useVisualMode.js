6447efe9cd7aaaed4db32ab4062b90bf422fd0be
Martha Staus <mjstaus@gmail.com>
2022-03-03 20:15:56 -0800

    Refactor to eliminate stale state for setHistory

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,27 +1,27 @@
-import {useState } from "react";
+import { useState } from "react";
 
 export function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+  const [mode, setMode] = useState(initial);
 
-  function transition(mode, replace=false) {
-    if(replace){
-      history.pop()
-    }
-    history.push(mode);
+  function transition(mode, replace = false) {
+    replace
+      ? setHistory((prev) => [...prev.slice(0, -1), mode])
+      : setHistory((prev) => [...prev, mode]);
     setMode(mode);
   }
 
   function back() {
-    if(history.length > 1){
-      history.pop();
-      setMode(history[history.length-1])
-    } 
+    setMode(history[0])
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, -1)]);
+      setMode(history[history.length-2])
+    }
   }
-  
+
   return {
     mode,
     transition,
     back,
-  }
+  };
 }
