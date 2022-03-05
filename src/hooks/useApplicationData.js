59d833af4f5d420aa3e7a4e7706022c434a03e3d
Martha Staus <mjstaus@gmail.com>
2022-03-05 08:39:53 -0800

    Move logic responsible for loading initial data + actions to update state from Application component  to useApplicationData hook

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function useApplicationData() {
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days", {
+        proxy: {
+          host: "localhost",
+          port: 8001,
+        },
+      }),
+      axios.get("/api/appointments", {
+        proxy: {
+          host: "localhost",
+          port: 8001,
+        },
+      }),
+      axios.get("/api/interviewers", {
+        proxy: {
+          host: "localhost",
+          port: 8001,
+        },
+      }),
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+      })
+      .catch((error) => {
+        console.log(error.status);
+        console.log(error.header);
+        console.log(error.data);
+      });
+  }, []);
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    const updateAppts = axios.put(`/api/appointments/${id}`, appointment);
+    updateAppts
+      .then(() => setState({ ...state, appointments: { ...appointments } }))
+      .catch((error) => {
+        console.log(error);
+      });
+    return updateAppts;
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    const deleteAppt = axios.delete(`/api/appointments/${id}`, appointment);
+    deleteAppt
+      .then(() => setState({ ...state, appointments: { ...appointments } }))
+      .catch((error) => {
+        console.log(error);
+      });
+    return deleteAppt;
+  }
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  }
+}
