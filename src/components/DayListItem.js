fd498a8895fdbcbf43a8ff07d7b3f6f465092911
Martha Staus <mjstaus@gmail.com>
2022-03-05 18:43:47 -0800

    CHORE: destructure props in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,10 +2,10 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
-export default function DayListItem(props) {
+export default function DayListItem({ selected, spots, setDay, name }) {
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots
+    "day-list__item--selected": selected,
+    "day-list__item--full": !spots
   });
 
   const formatSpots = (spots) => {
@@ -19,9 +19,9 @@ export default function DayListItem(props) {
   return (
     <li 
       className={dayClass}
-      onClick={props.setDay}>
-      <h2  className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      onClick={setDay}>
+      <h2  className="text--regular">{name}</h2> 
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
