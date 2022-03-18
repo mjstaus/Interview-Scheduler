f47efece23f9a7e946afcb23d2d26b8762493f46
Martha Staus <mjstaus@gmail.com>
2022-03-17 17:51:11 -0700

    TEST: Add data-testid property

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,7 +19,8 @@ export default function DayListItem({ selected, spots, setDay, name }) {
   return (
     <li 
       className={dayClass}
-      onClick={setDay}>
+      onClick={setDay}
+      data-testid="day">
       <h2  className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
