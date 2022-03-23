9b81b6b7c4425b42c2847c4040e9376e5670443a
Martha Staus <mjstaus@gmail.com>
2022-03-23 05:31:08 -0700

    TEST: Reinstall cypress locally. Add Navigation test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,13 @@
+// const { it } = require("mocha");
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
